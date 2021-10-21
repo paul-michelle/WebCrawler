cb8bbdfe617b14050fe4fcf2358796a76afe5019
Paul Michelle <paulmichellegallery@gmail.com>
2021-10-22 01:44:56 +0300

    webpage downscrolling solved and most items scraped

--- reddit-scraper.py
+++ reddit-scraper.py
@@ -1,13 +1,22 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+import uuid
 from time import sleep
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+load_dotenv()
 
+POSTS_FOR_PARSING_NUM = 100
+FAILED_SCRAPE_COEFF = 1,1
+SCROLLING_ITERATIONS = 100
+SLEEPING_INBETWEEN_SCROLLING = 0.07
 
+# SCROLLING_ITERATIONS = 3
+# SLEEPING_INBETWEEN_SCROLLING = 0.07
+# POSTS_FOR_PARSING_LIMIT = 5
 
 options = Options()
 options.headless = True
@@ -22,25 +31,53 @@ driver.get('https://www.reddit.com/r/all/top/?t=month')
 # driver.find_element(By.ID, 'loginPassword').send_keys(os.getenv("REDDIT_PASSWORD"))
 # driver.find_element(By.XPATH, '//button[@type="submit"]').click()
 
+scrollDown = "window.scrollBy(0,3000);"
+for i in range(SCROLLING_ITERATIONS):
+    sleep(SLEEPING_INBETWEEN_SCROLLING)
+    driver.execute_script(scrollDown)
 content = driver.page_source
+driver.quit()
 soup = BeautifulSoup(content, features="lxml")
 
-# for i in range(10):
-#     scrollDown = "window.scrollBy(0,2000);"
-#     driver.execute_script(scrollDown)
-#     sleep(1)
+posts = soup.findAll('div', attrs={"class":"Post"},
+                     limit=POSTS_FOR_PARSING_NUM*FAILED_SCRAPE_COEFF)
+posts_dict = {}
+# while len(posts_dict) != POSTS_FOR_PARSING_NUM:
+for index, post in enumerate(posts):
+    unique_id = uuid.uuid1().hex
+    post_url = post.find('a', attrs={"class": "_3jOxDPIQ0KaOWpzvSQo-1s"})['href']
+    username = post.find('a', attrs={"class": "_2tbHP6ZydRpjI44J3syuqC"}).contents[0].split("/")[1]
+    comments_num = post.find('span', attrs={"class": "FHCV02u6Cp2zYL0fhQPsO"}).contents[0].split()[0]
+    votes_num = post.find('div', attrs={"class": "_1rZYMD_4xY3gRcSS3p8ODO"}).contents[0]
+    category = post.find('div', attrs={"class": "_2mHuuvyV9doV3zwbZPtIPG"}).contents[0].contents[0].split("/")[1]
 
-anchors = soup.findAll('div', attrs={"class":"Post"}, limit=100)
-print(anchors)
+    user_url = f'https://www.reddit.com/{post.find("a", attrs={"class": "_2tbHP6ZydRpjI44J3syuqC"})["href"]}'
+    headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0)\
+                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36"
+               }
+    user_response = requests.get(user_url, headers=headers).content
+    user_soup = BeautifulSoup(user_response, features='lxml')
+    post_karma_span = user_soup.find('span', attrs={"class": "karma"})
+    if post_karma_span:
+        post_karma = post_karma_span.contents[0]
+    else:
+        post_karma = 'ignorepost'
+        continue
+    comment_karma_span = user_soup.find('span', attrs={"class": "comment-karma"})
+    comment_karma = comment_karma_span.contents[0]
 
-driver.quit()
+    posts_dict[index] = [post_url, post_karma, comment_karma]
 
-# for post in soup.select('.Post', limit=100):
-#     print(post)
-    # try:
-    #     print(post.select('._3jOxDPIQ0KaOWpzvSQo-1s')[0]['href'])
-    # except:
-    #     continue
+for key, value in posts_dict.items():
+    print(key,value)
+
+
+
+
+
+
+
+
 
 
 
